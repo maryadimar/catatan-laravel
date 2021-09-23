b1f24fefdf9c6006b9a34c53737a1f3657abcaa2
maryadi <maryadi071@gmail.com>
2021-09-23 17:52:55 +0700

    add srcipt bash II

--- bash/uninstall.sh
+++ bash/uninstall.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# script bash untuk uninstall di linux
+#------------------------------------
+# Created By Maryadi & Alif
+#------------------------------------
+
 apt remove --purge mysql-server
 apt purge mysql-server
 apt autoremove
